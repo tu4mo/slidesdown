c90838aa175e4c450a092251caf4fb59758e13d5
tu4mo <tu4mo@users.noreply.github.com>
2018-02-21 20:30:11 +0200

    Update default markdown

--- src/store.js
+++ src/store.js
@@ -5,12 +5,13 @@ import queryString from 'query-string'
 import { getSlides } from './firebase'
 
 const defaultMarkdown =
-  '# Welcome to *Slidesdown*\n\n---\n\n' +
-  '✨ Write markdown, get slides! ✨\n\n---\n\n' +
-  '## A list!\n\n- Awesome\n\n1. Yeah!'
+  '# ✨✨✨\n\n# Welcome to Slidesdown\n\n---\n\n' +
+  "## What is it?\n\nWith Slidesdown, you can write [markdown](https://en.wikipedia.org/wiki/Markdown) to create a **slideshow**.\n\n### Other features\n\n- No login required\n- Saves slides to your browser's localStorage\n- Code syntax highlighting\n- Sharing slides with unique URL\n\n---\n\n" +
+  '## How to get stared\n\n1. Start typing markdown on the editor\n2. Enjoy the realtime preview of slides\n3. Click *Presentation* to view them as a slideshow\n4. Share your slides!\n\n---\n\n' +
+  "## Some of the features\n\nCode syntax highlighting:\n```javascript\nconst hello = 'world'\nconsole.log(hello)\n```\n\nText formatting:\n\n- *Italic text*\n- **Bold text**\n- ~~Strikethrough text~~\n\n---\n\n" +
+  '# 👋\n\n# Have fun!'
 
-const getThemeFromUrl = () =>
-  queryString.parse(window.location.search).theme
+const getThemeFromUrl = () => queryString.parse(window.location.search).theme
 
 export const store = createStore({
   isLoading: false,
