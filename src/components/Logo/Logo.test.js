ad86b04d823e514d5908b802d1cb1d4708d06aa1
tu4mo <tu4mo@users.noreply.github.com>
2018-02-26 12:00:41 +0200

    Fix tests

--- src/components/Logo/Logo.test.js
+++ src/components/Logo/Logo.test.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
+import { ThemeProvider } from 'styled-components'
 
+import theme from '../../theme'
 import Logo from './Logo'
 
-jest.mock('./logo.svg', () => 'Logo');
+jest.mock('./logo.svg', () => 'Logo')
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Logo />).toJSON()
+  const tree = renderer
+    .create(
+      <ThemeProvider theme={theme}>
+        <Logo />
+      </ThemeProvider>
+    )
+    .toJSON()
 
   expect(tree).toMatchSnapshot()
 })
