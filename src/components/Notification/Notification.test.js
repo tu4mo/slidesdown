0d31d7f415b8b344867b5b79f300a5f9d0f4abc5
tu4mo <tu4mo@users.noreply.github.com>
2018-02-27 20:41:01 +0200

    Add test

---
+++ src/components/Notification/Notification.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import { ThemeProvider } from 'styled-components'
+
+import theme from '../../theme'
+import Notification from './Notification'
+
+jest.useFakeTimers()
+
+const component = renderer.create(
+  <ThemeProvider theme={theme}>
+    <Notification>Test</Notification>
+  </ThemeProvider>
+)
+
+it('renders correctly', () => {
+  expect(component.toJSON()).toMatchSnapshot()
+})
+
+it('renders nothing after time has passed', () => {
+  jest.runAllTimers()
+  expect(setTimeout).toHaveBeenCalledTimes(1)
+  expect(component.toJSON()).toMatchSnapshot()
+})
