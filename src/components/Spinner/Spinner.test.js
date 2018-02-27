30bf14e8f9cf6d1fa826dd5f30ce0a0a66d14e65
tu4mo <tu4mo@users.noreply.github.com>
2018-02-27 19:24:46 +0200

    Add snapshot

---
+++ src/components/Spinner/Spinner.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import { ThemeProvider } from 'styled-components'
+
+import theme from '../../theme'
+import Spinner from './Spinner'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <ThemeProvider theme={theme}>
+        <Spinner />
+      </ThemeProvider>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
