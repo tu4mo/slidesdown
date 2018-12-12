e58fcfab82af78924e556d62a2e966765df39e29
tu4mo <tu4mo@users.noreply.github.com>
2018-12-12 21:54:04 +0200

    Add logging

--- functions/presentation.js
+++ functions/presentation.js
@@ -14,6 +14,8 @@ module.exports = async (req, res, db) => {
       .doc(firstDocument.id)
       .update({ visitedAt: new Date() })
 
+    console.log(`${firstDocument.id}: visitedAt updated`)
+
     return res.send(firstDocument.data())
   } catch (err) {
     console.error(err)
