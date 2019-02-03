6b8390b629f911713862b548fe4ffc6345927d93
tu4mo <tu4mo@users.noreply.github.com>
2019-02-03 21:35:38 +0200

    Convert to TSX

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+
+import { StyledButton } from './Button.style'
+
+interface ButtonProps {
+  children: ReactNode
+  disabled: boolean
+  onClick(): void
+}
+
+const Button = ({ children, disabled, onClick }: ButtonProps) => (
+  <StyledButton disabled={disabled} onClick={onClick}>
+    {children}
+  </StyledButton>
+)
+
+export default Button
