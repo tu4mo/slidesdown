20d9ff6c0c677dd803dab3cc13eb1f7b1b29e631
tu4mo <tu4mo@users.noreply.github.com>
2019-03-05 20:34:55 +0200

    Remove obsolete setting

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 const db = admin.firestore()
-db.settings({ timestampsInSnapshots: true })
 
 const presentation = require('./presentation')
 const removeOldSlides = require('./removeOldSlides')
