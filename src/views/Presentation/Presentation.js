3cb22fbb9e5fa4a1d6a91d94cc0675c158023d06
tu4mo <tu4mo@users.noreply.github.com>
2019-07-21 11:40:49 +0300

    Convert to hooks

--- src/views/Presentation/Presentation.js
+++ src/views/Presentation/Presentation.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import Fullscreen from 'react-full-screen'
 
@@ -17,191 +17,170 @@ import {
   StyledPresentationToolbar
 } from './Presentation.style'
 
-class Presentation extends Component {
-  static propTypes = {
-    history: PropTypes.object.isRequired,
-    match: PropTypes.object.isRequired
-  }
+const Presentation = ({ history, match }) => {
+  const [isFullscreen, setIsFullscreen] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isToolbarVisible, setIsToolbarVisible] = useState(false)
+  const [markdown, setMarkdown] = useState('')
+  const [theme, setTheme] = useState('')
 
-  state = {
-    isFullscreen: false,
-    isLoading: true,
-    isToolbarVisible: false,
-    markdown: '',
-    theme: ''
-  }
+  const slidesCount = useRef(0)
+  const toolbarVisibilityTimer = useRef(null)
 
-  slidesCount = 0
-  toolbarVisibilityTimer = null
+  const changeSlide = useCallback(
+    (next = true) => {
+      const slideNumber = match.params.slideNumber || 0
+      const getSlideUrl = slideNumber =>
+        `/${match.params.slidesId || '-'}/${slideNumber}`
 
-  async componentDidMount() {
-    const { history, match } = this.props
-    const { slidesId } = match.params
+      if (next) {
+        if (slideNumber < slidesCount.current - 1) {
+          history.push(getSlideUrl(parseInt(slideNumber, 10) + 1))
+        }
+      } else {
+        if (slideNumber > 0) {
+          history.push(getSlideUrl(parseInt(slideNumber, 10) - 1))
+        }
+      }
+    },
+    [history, match.params.slidesId, match.params.slideNumber]
+  )
 
-    if (slidesId && slidesId !== '-') {
+  useEffect(() => {
+    const handleKeyUp = e => {
+      switch (e.code) {
+        case 'ArrowLeft':
+          changeSlide(false)
+          break
+        case 'ArrowRight':
+          changeSlide()
+          break
+        case 'Space': // Space
+          changeSlide()
+          break
+        default:
+          break
+      }
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+
+    return () => {
+      window.removeEventListener('keyup', handleKeyUp)
+      clearTimeout(toolbarVisibilityTimer.current)
+    }
+  }, [changeSlide])
+
+  useEffect(() => {
+    const slidesId = match.params.slidesId
+
+    const fetchPresentation = async () => {
       try {
         const slides = await getPresentation(slidesId)
 
-        this.setState({
-          isLoading: false,
-          markdown: slides.markdown,
-          theme: slides.theme
-        })
+        setIsLoading(false)
+        setMarkdown(slides.markdown)
+        setTheme(slides.theme)
       } catch (err) {
         history.push('/')
       }
     }
 
-    window.addEventListener('keyup', this.handleKeyUp)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keyup', this.handleKeyUp)
-    clearTimeout(this.toolbarVisibilityTimer)
-  }
-
-  changeSlide = (next = true) => {
-    const { history, match } = this.props
-    const { slidesId = '-', slideNumber = 0 } = match.params
-    const getSlideUrl = slideNumber => `/${slidesId}/${slideNumber}`
-
-    if (next) {
-      if (slideNumber < this.slidesCount - 1) {
-        history.push(getSlideUrl(parseInt(slideNumber, 10) + 1))
-      }
-    } else {
-      if (slideNumber > 0) {
-        history.push(getSlideUrl(parseInt(slideNumber, 10) - 1))
-      }
+    if (slidesId && slidesId !== '-') {
+      fetchPresentation()
     }
-  }
+  }, [history, match.params.slidesId])
 
-  close = () => {
-    const { history, match } = this.props
-    const { slidesId = '' } = match.params
-    history.push(`/${slidesId !== '-' ? slidesId : ''}`)
-  }
+  const handleSlidesCount = count => (slidesCount.current = count)
 
-  handleKeyUp = e => {
-    switch (e.keyCode) {
-      case 37: // Left arrow
-        this.changeSlide(false)
-        break
-      case 39: // Right arrow
-        this.changeSlide()
-        break
-      case 32: // Space
-        this.changeSlide()
-        break
-      default:
-        break
-    }
-  }
-
-  handleSlidesCount = count => (this.slidesCount = count)
-
-  handlePresentationMouseMove = e => {
-    if (!this.state.isToolbarVisible) {
-      this.setState({
-        isToolbarVisible: true
-      })
+  const handlePresentationMouseMove = () => {
+    if (!isToolbarVisible) {
+      setIsToolbarVisible(true)
     }
 
-    clearTimeout(this.toolbarVisibilityTimer)
+    clearTimeout(toolbarVisibilityTimer.current)
 
-    this.toolbarVisibilityTimer = setTimeout(
-      () =>
-        this.setState({
-          isToolbarVisible: false
-        }),
+    toolbarVisibilityTimer.current = setTimeout(
+      () => setIsToolbarVisible(false),
       2000
     )
   }
 
-  handleToolbarMouseMove = e => {
+  const handleToolbarMouseMove = e => {
     e.stopPropagation()
-    clearTimeout(this.toolbarVisibilityTimer)
+    clearTimeout(toolbarVisibilityTimer.current)
   }
 
-  render() {
-    const { match } = this.props
-    const {
-      isFullscreen,
-      isLoading,
-      isToolbarVisible,
-      markdown,
-      theme
-    } = this.state
-    const { slideNumber: slideNumberAsString = '0' } = match.params
-    const slideNumber = parseInt(slideNumberAsString, 10)
+  const { slideNumber: slideNumberAsString = '0' } = match.params
+  const slideNumber = parseInt(slideNumberAsString, 10)
 
-    return isLoading ? (
-      <Spinner />
-    ) : (
-      <Fullscreen
-        enabled={isFullscreen}
-        onChange={isFullscreen => this.setState({ isFullscreen })}
-      >
-        <StyledPresentation onMouseMove={this.handlePresentationMouseMove}>
-          <Slides
-            markdown={markdown}
-            onSlidesCount={this.handleSlidesCount}
-            singleSlide={slideNumber}
-            theme={theme}
-          />
-          <StyledNoticationContainer>
-            <Notification slideDown timeout={5000}>
-              Press space or arrows to change slide.
-            </Notification>
-          </StyledNoticationContainer>
-          <StyledPresentationToolbar
-            onMouseMove={this.handleToolbarMouseMove}
-            visible={isToolbarVisible}
-          >
-            <ToolBar>
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <Fullscreen
+      enabled={isFullscreen}
+      onChange={isFullscreen => setIsFullscreen(isFullscreen)}
+    >
+      <StyledPresentation onMouseMove={handlePresentationMouseMove}>
+        <Slides
+          markdown={markdown}
+          onSlidesCount={handleSlidesCount}
+          singleSlide={slideNumber}
+          theme={theme}
+        />
+        <StyledNoticationContainer>
+          <Notification slideDown timeout={5000}>
+            Press space or arrows to change slide.
+          </Notification>
+        </StyledNoticationContainer>
+        <StyledPresentationToolbar
+          onMouseMove={handleToolbarMouseMove}
+          visible={isToolbarVisible}
+        >
+          <ToolBar>
+            <Icon
+              disabled={slideNumber === 0}
+              onClick={() => changeSlide(false)}
+              tooltip={
+                <span>
+                  Previous <Key>←</Key>
+                </span>
+              }
+              type="left"
+            />
+            {isFullscreen ? (
               <Icon
-                disabled={slideNumber === 0}
-                onClick={() => this.changeSlide(false)}
-                tooltip={
-                  <span>
-                    Previous <Key>←</Key>
-                  </span>
-                }
-                type="left"
+                onClick={() => setIsFullscreen(false)}
+                tooltip={<span>Minimize</span>}
+                type="minimize"
               />
-              {isFullscreen ? (
-                <Icon
-                  onClick={() =>
-                    this.setState({
-                      isFullscreen: false
-                    })
-                  }
-                  tooltip={<span>Minimize</span>}
-                  type="minimize"
-                />
-              ) : (
-                <Icon
-                  onClick={() => this.setState({ isFullscreen: true })}
-                  tooltip={<span>Maximize</span>}
-                  type="maximize"
-                />
-              )}
+            ) : (
               <Icon
-                disabled={slideNumber >= this.slidesCount - 1}
-                onClick={this.changeSlide}
-                tooltip={
-                  <span>
-                    Next <Key>→</Key> / <Key>space</Key>
-                  </span>
-                }
-                type="right"
+                onClick={() => setIsFullscreen(true)}
+                tooltip={<span>Maximize</span>}
+                type="maximize"
               />
-            </ToolBar>
-          </StyledPresentationToolbar>
-        </StyledPresentation>
-      </Fullscreen>
-    )
-  }
+            )}
+            <Icon
+              disabled={slideNumber >= slidesCount.current - 1}
+              onClick={changeSlide}
+              tooltip={
+                <span>
+                  Next <Key>→</Key> / <Key>space</Key>
+                </span>
+              }
+              type="right"
+            />
+          </ToolBar>
+        </StyledPresentationToolbar>
+      </StyledPresentation>
+    </Fullscreen>
+  )
+}
+
+Presentation.propTypes = {
+  history: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired
 }
 
 export default Presentation
