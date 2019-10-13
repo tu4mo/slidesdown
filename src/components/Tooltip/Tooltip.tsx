a5d9d71413c65ed9ea78ae8a231123a4d746bb87
tu4mo <tu4mo@users.noreply.github.com>
2019-10-13 14:24:43 +0300

    Update Tippy

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import Tippy, { TippyProps } from '@tippy.js/react'
+import 'tippy.js/dist/tippy.css'
 
 const Tooltip: FC<TippyProps> = ({ children, content, ...rest }) => (
   <Tippy arrow content={content} {...rest}>
