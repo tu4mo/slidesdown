9c5c84ac8dc8a715728b3af99ca266e214151cc5
tu4mo <tu4mo@users.noreply.github.com>
2020-04-23 22:03:00 +0300

    Fix proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,9 @@
-const proxy = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (app) => {
   app.use(
-    proxy('/api', {
+    '/api',
+    createProxyMiddleware({
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
       secure: false,
