ae5acec1dc0b7088241f23df97cccd16c43608ec
tu4mo <tu4mo@users.noreply.github.com>
2024-12-07 14:55:29 +0200

    Update firebase-functions

--- functions/src/presentation.ts
+++ functions/src/presentation.ts
@@ -1,5 +1,5 @@
 import * as admin from 'firebase-admin'
-import * as functions from 'firebase-functions'
+import * as functions from 'firebase-functions/v1'
 
 export const presentation = async (
   req: functions.Request,
