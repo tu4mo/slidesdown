f208c84c77039212d0e041f1af86f784f7f26392
tu4mo <tu4mo@users.noreply.github.com>
2025-09-13 12:12:32 +0300

    No need to create a variable

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,4 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
-const root = createRoot(document.getElementById('root')!)
-root.render(<App />)
+createRoot(document.getElementById('root')!).render(<App />)
