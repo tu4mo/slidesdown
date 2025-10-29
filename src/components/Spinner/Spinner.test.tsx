0741cb7005b0c0e1faa04355036525f6cb0653a5
tu4mo <tu4mo@users.noreply.github.com>
2025-10-29 22:09:17 +0200

    Convert to CSS modules

--- src/components/Spinner/Spinner.test.tsx
+++ src/components/Spinner/Spinner.test.tsx
@@ -7,7 +7,7 @@ it('renders correctly', () => {
   expect(asFragment()).toMatchInlineSnapshot(`
     <DocumentFragment>
       <div
-        class="sc-bRKDuR ctSWrx"
+        class="_spinner_12c993"
       />
     </DocumentFragment>
   `)
