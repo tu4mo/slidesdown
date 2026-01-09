4c91ab55989d1167320aa8e8329593259d1e8aab
tu4mo <tu4mo@users.noreply.github.com>
2026-01-09 16:00:47 +0200

    Remove obsolete deps

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,5 @@
 export default {
   extends: ['stylelint-config-recommended'],
-  overrides: [
-    {
-      files: ['**/*.tsx', '**/*.ts'],
-      customSyntax: 'postcss-styled-syntax',
-    },
-  ],
   rules: {
     'function-no-unknown': null,
     'media-query-no-invalid': null,
